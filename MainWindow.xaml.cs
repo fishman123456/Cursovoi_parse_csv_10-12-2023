5912728588962e3a2fab073a97bf20a610dbb91f
fishman123456 <den9152@yandex.ru>
2023-12-10 19:00:53 +0700

    10-12-2023

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Cursovoi_parse_csv_10_12_2023
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ListBox list = new ListBox();
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+        ObservableCollection<string> m_osstrProgress;  // To attach to the listbox.  
+        private void buttOpenFileDialog_Click(object sender, RoutedEventArgs e)
+        {
+            openFileCSV openFilecsv = new openFileCSV();
+            openFilecsv.openFileDialog(list);
+            lbFiles.Text = openFilecsv.filenameGO;
+        }
+    }
+}
---
+++ openFileCSV.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.ObjectModel;
+
+namespace Cursovoi_parse_csv_10_12_2023
+{
+    // открываем файл csv для чтения
+    public class openFileCSV
+    {
+        public ObservableCollection<string> csv;
+        public string filenameGO;
+        public string openFileDialog(ListBox lbFiles)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Text files (*.csv)|*.csv|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                {
+                    lbFiles.Items.Add(Path.GetFileName(filename));
+                    filenameGO = filename;
+                }
+            }
+            return filenameGO;
+        }
+    }
+}
